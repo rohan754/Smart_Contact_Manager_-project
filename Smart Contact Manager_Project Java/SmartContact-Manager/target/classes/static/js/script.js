fb0f13c3d4a4d6feb4d75ec16ff1e07c94481149
rohan754 <70831273+rohan754@users.noreply.github.com>
2021-08-24 13:44:42 +0530

    Complete File Directory

---
+++ Smart Contact Manager_Project Java/SmartContact-Manager/target/classes/static/js/script.js
@@ -0,0 +1,25 @@
+console.log("This is Script file");
+
+const toggleSidebar=()=>{
+	
+	if($(".sidebar").is(":visible")) {
+		//true		
+		//band karna hai
+		
+		$(".sidebar").css("display", "none");
+		$(".content").css("margin-left", "0%");
+		
+		
+		
+	} else{
+		//false
+		//show karna hai
+		
+		$(".sidebar").css("display", "block");
+		$(".content").css("margin-left", "20%");
+		
+		
+	}
+	
+	
+};
